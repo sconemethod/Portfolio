8b124fc4f715b188c2549209c4f7f917b003df87
sconeq <sconezq@gmail.com>
2022-10-10 22:27:03 +0900

    JS : home contact btn

--- main.js
+++ main.js
@@ -22,7 +22,6 @@ document.addEventListener('scroll', ()=>{
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
- 
     //  아래 조건 쓰기전에는 menu 바 모두(버튼 아닌애들까지) 클릭됐을때 결과를 도출하기 떄문에 값이 없는 애들도 있다.
     //console.log(event.target.dataset.link);
 const target = event.target;
@@ -33,7 +32,42 @@ if(link == null){
 }
 // 버튼 애들만 할당이 된다.
 console.log(event.target.dataset.link);
-const scrollTo = document.querySelector(link);
-//이동
-scrollTo.scrollIntoView({ behavior: 'smooth' });
+// const scrollTo = document.querySelector(link);
+// //이동
+// scrollTo.scrollIntoView({ behavior: 'smooth' });
+
+scrollIntoViews(link);
 })
+
+
+// Handle click on "contact me" button on home //엘리썜 버전
+const homeContactBtn = document.querySelector('.home__contact');
+    homeContactBtn.addEventListener('click', () => {
+        scrollIntoViews('#contact')
+
+});
+
+// Handle click on "contact me" button on home // 내 버전
+// const homeContactBtn = document.querySelector('.home__contact');
+//     homeContactBtn.addEventListener('click', (event) => {
+//         console.log(`event: ${event}`); //PointerEvent
+//         console.log(`event.target: ${event.target}`); // 버튼
+//         // console.log(`target: ${target}`); // 없음 
+
+//     const target = event.target; 
+//     const link = target.dataset.link; // 아래 (link) 쓰고 싶으면 이름 여기서 지어주어야 함
+//         console.log(`target: ${target}`); //[object HTMLButtonElement]
+//         console.log(`target.dataset: ${target.dataset}`); //[object DOMStringMap]
+//         console.log(`target.dataset.link : ${target.dataset.link}`); //#contact
+
+//     // link 값으로 이동
+//     const scrollTo = document.querySelector(link);
+//     scrollTo.scrollIntoView({ behavior: 'smooth' });
+
+// })
+
+
+function scrollIntoViews(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
